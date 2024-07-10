83d480bdffa524bb0a88ae59553508268389e742
UditKumar Chauhan <udichauhan@deloitte.com>
2024-07-10 17:28:33 +0530

    Changes-10-07-2024

---
+++ dist/index.7c0ccee6.js
@@ -0,0 +1,8 @@
+const hieading2 = React.createElement("h1", {
+    id: "heading1"
+}, "H1 tag from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(hieading2);
+console.log(hieading2);
+
+//# sourceMappingURL=index.7c0ccee6.js.map
