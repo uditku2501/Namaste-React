193c908507450aa6104574e1dd6b5766b51ca628
UditKumar Chauhan <udichauhan@deloitte.com>
2024-07-10 12:50:38 +0530

    Episode -01

---
+++ app.js
@@ -0,0 +1,9 @@
+const hieading2 = React.createElement(
+    "h1",
+    {id:"heading1"},
+    "H1 tag from React"
+    );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(hieading2);
+
+console.log(hieading2);
